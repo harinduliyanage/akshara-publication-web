90a5c36410bec921eaf44f10e07809cfd90d6abb
Harindu Liyanage <harindu.sul@gmail.com>
2020-10-10 19:28:22 +0530

    updated

---
+++ src/app/app.component.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+import './app.component.scss';
+
+class AppComponent extends Component{
+
+  render() {
+      return (
+          <>
+
+          </>
+      );
+  }
+}
+
+export default AppComponent;
